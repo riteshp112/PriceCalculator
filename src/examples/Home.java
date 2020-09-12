df8b72cf0a938c824cb050887644b2f23ca4dbb3
riteshp112 <50243841+riteshp112@users.noreply.github.com>
2020-09-12 21:43:09 +0530

    Update Home.java

--- src/examples/Home.java
+++ src/examples/Home.java
@@ -2,8 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 class Home extends JFrame implements ActionListener
-{ System.setProperty("java.awt.headless", "false"); //Disables headless
-JLabel j;
+{ JLabel j;
     JLabel j1,j2;
     JButton cp,au,cu,ex;
     public Home()
